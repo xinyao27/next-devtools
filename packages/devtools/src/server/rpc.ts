a7e5ee45f8e04c56bc527b09a962be1b135ca3b0
Xin Yao <hi@xinyao.me>
2024-01-01 20:48:53 +0800

    chore: remove rpc connection logs

--- packages/devtools/src/server/rpc.ts
+++ packages/devtools/src/server/rpc.ts
@@ -1,6 +1,5 @@
 import ws from 'ws'
 import { applyWSSHandler } from '@trpc/server/adapters/ws'
-import consola from 'consola'
 import { type WebpackOptionsNormalized } from 'webpack'
 import { RPC_SERVER_PORT } from '@next-devtools/shared'
 import { type Context } from '@next-devtools/shared'
@@ -11,15 +10,7 @@ export function createRPCServer(options: WebpackOptionsNormalized, context: Cont
   const wss = new ws.Server({ port: Number(RPC_SERVER_PORT) })
   const handler = applyWSSHandler({ wss, router: appRouter, createContext: createContext(options, context) })
 
-  wss.on('connection', (ws) => {
-    consola.log(`➕➕ Connection (${wss.clients.size})`)
-    ws.once('close', () => {
-      consola.log(`➖➖ Connection (${wss.clients.size})`)
-    })
-  })
-
   process.on('SIGTERM', () => {
-    consola.log('SIGTERM')
     handler.broadcastReconnectNotification()
     wss.close()
   })
