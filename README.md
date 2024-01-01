cc31cb1e833e7fedc2e7c3fde06b4027bb4953a8
Xin Yao <hi@xinyao.me>
2024-01-02 00:00:17 +0800

    feat: add examples dir

--- README.md
+++ README.md
@@ -8,17 +8,17 @@ Using Next DevTools in your Next project.
 
 You can refer to these two projects to use Next DevTools.
 
-- [app-router](./playgrounds/app-router/)
-- [pages-router](./playgrounds/pages-router/)
+- [app-router](./examples/app-router/)
+- [pages-router](./examples/pages-router/)
 
 You can create a Next.js project using Next DevTools with the following.
 
 ```bash
 # app-router
-npx create-next-app@latest nextjs-devtools-app-router --use-npm --example "https://github.com/xinyao27/next-devtools/tree/main/playgrounds/app-router"
+npx create-next-app@latest nextjs-devtools-app-router --use-npm --example "https://github.com/xinyao27/next-devtools/tree/main/examples/app-router"
 
 # pages-router
-npx create-next-app@latest nextjs-devtools-pages-router --use-npm --example "https://github.com/xinyao27/next-devtools/tree/main/playgrounds/pages-router"
+npx create-next-app@latest nextjs-devtools-pages-router --use-npm --example "https://github.com/xinyao27/next-devtools/tree/main/examples/pages-router"
 ```
 
 ### Installation
---
+++ examples/app-router/src/app/page.tsx
@@ -0,0 +1,95 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            By{' '}
+            <Image
+              priority
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              height={24}
+              src="/vercel.svg"
+              width={100}
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          priority
+          alt="Next.js Logo"
+          className={styles.logo}
+          height={37}
+          src="/next.svg"
+          width={180}
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          className={styles.card}
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          className={styles.card}
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          className={styles.card}
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          className={styles.card}
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
