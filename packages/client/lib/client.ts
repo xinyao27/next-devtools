3168d7d9a621e5c981d9ff2b46b4a764018e538e
Xin Yao <hi@xinyao.me>
2024-01-11 14:42:35 +0800

    fix(client): add types to useRPCClient hook

--- packages/client/lib/client.ts
+++ packages/client/lib/client.ts
@@ -3,8 +3,11 @@
 import { useRef } from 'react'
 import { type FrameMessageHandler, createFrameMessageClient } from '@next-devtools/shared'
 import { createRPCClient } from '@/lib/rpc'
+import type { MutableRefObject } from 'react'
+import type { CreateTRPCProxyClient } from '@trpc/client'
+import type { AppRouter } from '@next-devtools/core/types'
 
-export function useRPCClient() {
+export function useRPCClient(): MutableRefObject<CreateTRPCProxyClient<AppRouter> | null> {
   const client = useRef(createRPCClient())
   return client
 }
