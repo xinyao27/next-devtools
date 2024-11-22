f0ef056564e256f3a49cb537fa6053d7829adfa1
Xinyao <hi@xinyao.me>
2024-11-23 00:31:53 +0800

    fix: wrong packageDir/distDir

--- packages/core/src/dirs.ts
+++ packages/core/src/dirs.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'node:path'
 
-export const packageDir = resolve(__dirname, '../..')
-export const distDir = resolve(__dirname, '../dist')
+export const packageDir = resolve(__dirname, '../../..')
+export const distDir = resolve(__dirname, '../../dist')
 export const clientDir =
   process.env.NODE_ENV === 'development' ? resolve(packageDir, 'client') : resolve(distDir, 'client')
