23ff5a8eb383a3da752b2f22c90542c4f6ed87d5
Xinyao <hi@xinyao.me>
2024-11-25 16:08:22 +0800

    feat(client): settings refetch data

--- packages/client/app/settings/(components)/actions.tsx
+++ packages/client/app/settings/(components)/actions.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
+import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
+import { api } from '@/lib/client'
 
 export default function Actions() {
-  // const handleRefetchData = () => {
+  const utils = api.useUtils()
 
-  // }
-
-  const handleReloadPage = () => {
-    // router.refresh()
+  const handleReloadPage = React.useCallback(() => {
     window.location.reload()
-  }
+  }, [])
+
+  const handleRefetchData = React.useCallback(() => {
+    utils.invalidate()
+    toast.success('Data refetched')
+  }, [utils])
 
   return (
     <section className="space-y-2">
       <h2 id="actions">Actions</h2>
-      <div className="rounded border p-4">
-        {/* <Button>Refetch Data</Button> */}
+      <div className="flex flex-wrap gap-2 rounded border p-4">
         <Button variant="outline" onClick={handleReloadPage}>
           <i className="i-ri-refresh-line mr-2 size-4" />
           Reload Page
+        </Button>
+
+        <Button variant="outline" onClick={handleRefetchData}>
+          <i className="i-ri-refresh-line mr-2 size-4" />
+          Refetch Data
         </Button>
       </div>
     </section>
