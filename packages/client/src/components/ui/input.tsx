3d65c4068f0cda9504ad9fa88df1912a39bd3d47
Xinyao <hi@xinyao.me>
2024-12-07 13:15:48 +0800

    refactor: enhancing styling

--- packages/client/src/components/ui/input.tsx
+++ packages/client/src/components/ui/input.tsx
@@ -1,39 +1,25 @@
 import * as React from 'react'
-
 import { cn } from '@/lib/utils'
 
-export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'prefix'> {
-  prefix?: React.ReactNode
-}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, prefix, ...props }, ref) => {
-  return (
-    <div className={cn('relative', { 'cursor-not-allowed opacity-50': props.disabled })}>
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
+  ({ className, type, ...props }, ref) => {
+    return (
       <input
         ref={ref}
         type={type}
         className={cn(
-          'border-input bg-background text-foreground ring-offset-background placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:ring-ring/30 peer flex h-8 w-full rounded-lg border px-3 py-2 text-sm shadow-sm shadow-black/5 transition-shadow focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+          'border-input bg-background text-foreground placeholder:text-muted-foreground/70 focus-visible:border-ring focus-visible:ring-ring/20 flex h-9 w-full rounded-lg border px-3 py-2 text-sm shadow-sm shadow-black/5 transition-shadow focus-visible:outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
           type === 'search' &&
             '[&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-decoration]:appearance-none [&::-webkit-search-results-button]:appearance-none [&::-webkit-search-results-decoration]:appearance-none',
           type === 'file' &&
             'text-muted-foreground/70 file:border-input file:text-foreground p-0 pr-3 italic file:me-3 file:h-full file:border-0 file:border-r file:border-solid file:bg-transparent file:px-3 file:text-sm file:font-medium file:not-italic',
-          {
-            'ps-9': !!prefix,
-          },
           className,
         )}
         {...props}
       />
-
-      {prefix ? (
-        <div className="text-muted-foreground/80 pointer-events-none absolute inset-y-0 start-0 flex items-center justify-center ps-3 peer-disabled:opacity-50">
-          {prefix}
-        </div>
-      ) : null}
-    </div>
-  )
-})
+    )
+  },
+)
 Input.displayName = 'Input'
 
 export { Input }
